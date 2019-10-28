2892536682e531c9782b3bdac6a6fbd77542d229
g-morishita <40911178+g-morishita@users.noreply.github.com>
2019-10-28 11:36:34 +0900

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -128,4 +128,3 @@ def score_lr_fsiw(data, chose_model, debug=False):
 if __name__=='__main__':
     data = read_raw_data()
     scrs =score_lr_fsiw(data, 'fsiw', False)
-    import pdb; pdb.set_trace()
